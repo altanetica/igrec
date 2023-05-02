e8fe757d41ec2e517a36ae8fc555e1235ff28e03
Unit <unit.adsl@gmail.com>
2023-05-02 17:53:02 +0200

    rework to dict of func

--- src/interfaces/rpc.py
+++ src/interfaces/rpc.py
@@ -8,30 +8,91 @@ from src.interfaces.zeromq import zeromq_request_response as rr
 from src.interfaces.zeromq.shared.requestobject import RequestObject
 from src.interfaces.zeromq.shared.responseobject import ResponseSuccess, ResponseFail
 
+import src.use_cases.arp as arp
+import src.use_cases.auth as auth
+import src.use_cases.self as self
+import src.use_cases.whitelist as whitelist
 
 MODULES = ('arp', 'auth', 'self', 'whitelist')
 ACTIONS = ('add', 'remove', 'flush', 'enable', 'disable', 'load',
            'check', 'list', 'status', 'counters', 'test', 'save')
+FN = {
+    'arp': {
+        'add': arp.add.execute,
+        'remove': arp.remove.execute,
+        'flush': arp.flush.execute,
+        'enable': arp.enable.execute,
+        'disable': arp.disable.execute,
+        'load': arp.load.execute,
+        'check': arp.check.execute,
+        'list': arp.list.execute,
+        'status': arp.status.execute,
+        'counters': arp.counters.execute,
+        'test': arp.test.execute,
+        'save': arp.save.execute,
+    },
+    'auth': {
+        'add': auth.add.execute,
+        'remove': auth.remove.execute,
+        'flush': auth.flush.execute,
+        'enable': auth.enable.execute,
+        'disable': auth.disable.execute,
+        'load': auth.load.execute,
+        'check': auth.check.execute,
+        'list': auth.list.execute,
+        'status': auth.status.execute,
+        'counters': auth.counters.execute,
+        'test': auth.test.execute,
+        'save': auth.save.execute,
+    },
+    'self': {
+        'add': self.add.execute,
+        'remove': self.remove.execute,
+        'flush': self.flush.execute,
+        'enable': self.enable.execute,
+        'disable': self.disable.execute,
+        'load': self.load.execute,
+        'check': self.check.execute,
+        'list': self.list.execute,
+        'status': self.status.execute,
+        'counters': self.counters.execute,
+        'test': self.test.execute,
+        'save': self.save.execute,
+    },
+    'whitelist': {
+        'add': whitelist.add.execute,
+        'remove': whitelist.remove.execute,
+        'flush': whitelist.flush.execute,
+        'enable': whitelist.enable.execute,
+        'disable': whitelist.disable.execute,
+        'load': whitelist.load.execute,
+        'check': whitelist.check.execute,
+        'list': whitelist.list.execute,
+        'status': whitelist.status.execute,
+        'counters': whitelist.counters.execute,
+        'test': whitelist.test.execute,
+        'save': whitelist.save.execute,
+    },
+}
 
-
+'''
 def load_modules():
     loaded_modules = dict()
     for module in MODULES:
         pkg = "src.use_cases." + module
         use_cases = dict()
+        print(pkg)
         for use_case in ACTIONS:
             try:
                 m = importlib.import_module(use_case, pkg)
                 use_cases[use_case] = m
             except ImportError as e:
-                logging.warning(e)
+                logging.warning("Import Error:", e)
             except Exception as e:
                 logging.warning("Unexpected error:", e)
         loaded_modules[module] = use_cases
     return loaded_modules
-
-
-LOADED_MODULES = load_modules()
+'''
 
 
 # calling use case without answer needed
@@ -40,7 +101,7 @@ def controller(req):
     :param req:
     :type req: RequestObject
     """
-    global LOADED_MODULES
+    global FN
     if not isinstance(req, RequestObject):
         logging.warning("Request is invalid")
         return
@@ -53,11 +114,10 @@ def controller(req):
         logging.warning(req.action + " not in action list")
         return
 
-    module = LOADED_MODULES.get(req.module_, None).get(req.action, None)
-    if module is not None:
+    func = FN.get(req.module_, None).get(req.action, None)
+    if func is not None:
         try:
-            module.execute(req.data)
-            #getattr(m, "execute")(req.data)
+            func(req.data)
         except (AttributeError, TypeError, ValueError) as e:
             logging.warning(e)
         except NotImplementedError as e:
@@ -72,7 +132,7 @@ def informer(req):
     :param req:
     :type req: RequestObject
     """
-    global LOADED_MODULES
+    global FN
     if not isinstance(req, RequestObject):
         logging.warning("Request is invalid")
         return ResponseFail(req, "Request is invalid")
@@ -84,10 +144,10 @@ def informer(req):
     if req.action not in ACTIONS:
         return ResponseFail(req.action, "Not in action list")
 
-    module = LOADED_MODULES.get(req.module_, None).get(req.action, None)
-    if module is not None:
+    func = FN.get(req.module_, None).get(req.action, None)
+    if func is not None:
         try:
-            resp = module.execute(req.data)
+            resp = func(req.data)
             return ResponseSuccess(resp)
             #getattr(m, "execute")(req.data)
         except (AttributeError, TypeError, ValueError) as e:
