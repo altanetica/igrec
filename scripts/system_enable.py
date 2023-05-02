e76c02575db63fdb3f1119625ad14bc8a832afb0
Unit <unit.adsl@gmail.com>
2023-05-02 11:06:15 +0200

    move to scripts

--- scripts/system_enable.py
+++ scripts/system_enable.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from src.use_cases.app import enable as uc
+from src.use_cases.self import enable as uc
 
 if __name__ == '__main__':
     uc.process_request()
--- system_disable.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from src.use_cases.app import disable as uc
-
-if __name__ == '__main__':
-    uc.process_request()
